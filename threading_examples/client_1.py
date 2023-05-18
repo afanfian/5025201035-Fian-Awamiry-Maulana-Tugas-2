5dc8159b791fd3b72eaebfd18643737dd736c084
afanfian <afanfian14@gmail.com>
2023-05-18 20:45:46 +0700

    feat: add time in client_1.py

--- threading_examples/client_1.py
+++ threading_examples/client_1.py
@@ -1,4 +1,3 @@
-import sys
 import socket
 import logging
 
@@ -7,23 +6,17 @@ import logging
 def kirim_data():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     logging.warning("membuka socket")
-
     server_address = ('localhost', 45000)
     logging.warning(f"opening socket {server_address}")
     sock.connect(server_address)
 
     try:
-        # Send data
-        message = 'INI ADALAH DATA YANG DIKIRIM ABCDEFGHIJKLMNOPQ'
-        logging.warning(f"[CLIENT] sending {message}")
-        sock.sendall(message.encode())
-        # Look for the response
-        amount_received = 0
-        amount_expected = len(message)
-        while amount_received < amount_expected:
-            data = sock.recv(16)
-            amount_received += len(data)
-            logging.warning(f"[DITERIMA DARI SERVER] {data}")
+        msg = 'TIME\r\n'
+        logging.warning(f"sending {msg}")
+        sock.sendall(msg.encode('utf-8'))
+        data = sock.recv(32)
+        result = data.decode('utf-8')
+        logging.warning(f"{result}")
     finally:
         logging.warning("closing")
         sock.close()
