682fa622ec15740e17a8dce5b8cdb8c4a4872369
afanfian <afanfian14@gmail.com>
2023-05-18 21:26:49 +0700

    feat: change time in server

--- server.py
+++ server.py
@@ -1,55 +1,56 @@
-from socket import *
 import socket
 import threading
 import logging
 import time
 
-class ProcessTheClient(threading.Thread):
-	def __init__(self,connection,address):
-		self.connection = connection
-		self.address = address
-		threading.Thread.__init__(self)
+class ClientProcessor(threading.Thread):
+    def __init__(self, connection, address):
+        super().__init__()
+        self.connection = connection
+        self.address = address
 
-	def run(self):
-		while True:
-			data = self.connection.recv(32)
-			if data:
-				logging.warning(
-					f"[TIME SERVER] received {data} from {self.address}")
-				if data.startswith(b'TIME') and data.endswith(b'\r\n'):
-					current_time = time.strftime("%H:%M:%S")
-					response = f"JAM {current_time}\r\n"
-					logging.warning(
-						f"[TIME SERVER] sending {response} to {self.address}"
-					)
-					self.connection.sendall(response.encode('utf-8'))
-				else:
-					break
-			else:
-				break
-		self.connection.close()
+    def run(self):
+        while True:
+            data = self.connection.recv(32)
+            if not data:
+                break
 
-class Server(threading.Thread):
-	def __init__(self):
-		self.the_clients = []
-		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		threading.Thread.__init__(self)
+            logging.warning(f"[TIME SERVER] received {data} from {self.address}")
+            if data.startswith(b'TIME') and data.endswith(b'\r\n'):
+                current_time = time.strftime("%H:%M:%S")
+                response = f"JAM {current_time}\r\n"
+                logging.warning(f"[TIME SERVER] sending {response} to {self.address}")
+                self.connection.sendall(response.encode('utf-8'))
+            else:
+                break
 
-	def run(self):
-		self.my_socket.bind(('0.0.0.0', 45000))
-		self.my_socket.listen(1)
-		while True:
-			self.connection, self.client_address = self.my_socket.accept()
-			logging.warning(f"connection from {self.client_address}")
+        self.connection.close()
 
-			clt = ProcessTheClient(self.connection, self.client_address)
-			clt.start()
-			self.the_clients.append(clt)
+class TimeServer(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self.clients = []
+        self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    def run(self):
+        self.my_socket.bind(('0.0.0.0', 45000))
+        self.my_socket.listen(1)
+        while True:
+            connection, client_address = self.my_socket.accept()
+            logging.warning(f"connection from {client_address}")
+
+            client_processor = ClientProcessor(connection, client_address)
+            client_processor.start()
+            self.clients.append(client_processor)
+
+    def stop(self):
+        self.my_socket.close()
 
 def main():
-	svr = Server()
-	svr.start()
+    logging.basicConfig(level=logging.WARNING)
+    server = TimeServer()
+    server.start()
+    server.join()
 
-if __name__=="__main__":
-	main()
+if __name__ == "__main__":
+    main()
