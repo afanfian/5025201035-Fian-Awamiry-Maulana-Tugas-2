f9239f7e36ae2686a587099d3e316a3a5e4ad5da
afanfian <afanfian14@gmail.com>
2023-05-18 20:16:24 +0700

    feat: change port client_1

--- threading_examples/client_1.py
+++ threading_examples/client_1.py
@@ -8,7 +8,7 @@ def kirim_data():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     logging.warning("membuka socket")
 
-    server_address = ('localhost', 10000)
+    server_address = ('localhost', 45000)
     logging.warning(f"opening socket {server_address}")
     sock.connect(server_address)
 
